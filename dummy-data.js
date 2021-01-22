74e9aa4d268970cbfc4121e61cb423ec55caa6db
longvb <vuongbaolong48@gmail.com>
2021-01-22 17:36:27 +0700

    add theme

--- dummy-data.js
+++ dummy-data.js
@@ -3,7 +3,7 @@ module.exports = {
         name: "longvb",
         money_format: "d",
         money_with_currency_format: "d",
-        currency: "vnd",
+        currency: "VND",
     },
     products: {
         product: {
